1d30dbd496bfac8f7df300fd6a69f1b33738b4d5
varchunn <varvaram186@gmail.com>
2024-10-25 01:26:10 +0300

    Initial Commit

---
+++ bankomat/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bankomat
+//
+//  Created by Варя Мінєнкова on 25.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
